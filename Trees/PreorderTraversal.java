61bfefeac6a1d87b84cf5d8bc7bafa3157431578
knight-Security <paliwaladitya24@gmail.com>
2026-07-17 15:56:39 +0530

    Binary Tree preorder traversal

---
+++ Trees/PreorderTraversal.java
@@ -0,0 +1,48 @@
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data= data;
+        this.left= null;
+        this.right=null;
+    }
+}
+class BinaryTree{
+    Node root;
+    Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        if(data<root.data){
+            root.left=insert(root.left,data);
+        }
+        else{
+            root.right=insert(root.right,data);
+        }
+        return root;
+    }
+    void preorder(Node root){
+        if(root==null){
+            return;
+        }
+       System.out.print(root.data+" ");
+       preorder(root.left);
+       preorder(root.right);
+    }
+}
+class Main {
+    public static void main(String[] args) {
+       BinaryTree tree = new BinaryTree();
+
+        tree.root = tree.insert(tree.root, 50);
+        tree.root = tree.insert(tree.root, 30);
+        tree.root = tree.insert(tree.root, 70);
+        tree.root = tree.insert(tree.root, 20);
+        tree.root = tree.insert(tree.root, 40);
+        tree.root = tree.insert(tree.root, 60);
+        tree.root = tree.insert(tree.root, 80);
+
+        tree.preorder(tree.root);
+    }
+}
